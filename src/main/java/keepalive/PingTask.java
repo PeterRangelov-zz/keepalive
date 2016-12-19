f83156f4d6856424efde4fd2088d4340b914ea3d
Peter Rangelov <peter.rangelov11@gmail.com>
2016-12-18 22:09:25 -0500

    Optimization

--- src/main/java/keepalive/PingTask.java
+++ src/main/java/keepalive/PingTask.java
@@ -14,53 +14,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class PingTask {
     private static final Logger log = LoggerFactory.getLogger(PingTask.class);
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     
-    
-
     @Scheduled(fixedRate = 3600000)
     private void reportCurrentTime() {
-//        log.info("The time is now {}", dateFormat.format(new Date()));
     	sendPing();
     }
     
     private void sendPing() {
     	WebDriver driver = new HtmlUnitDriver(false);
-//    	
     	ArrayList<String> hosts = new ArrayList<String>(Arrays.asList(System.getenv("hosts").split(",")));
     	
     	for (String host : hosts) {
     		log.info("Pinging host " + host);        	
-        	driver.get("https://" + host);
+        	driver.get("https://" + host + ".herokuapp.com");
         	log.info("Page title: " + driver.getTitle());
     	}
     	
-    	
-    	
-//    	boolean isReachable;
-//		try {
-//			isReachable = InetAddress.getByName(host).isReachable(20);
-//			
-//			if (isReachable) {
-//	    		log.info("Pinged ");
-//	    	} else {
-//	    		log.error("Couldn't ping");
-//	    	}
-//			
-//		} catch (UnknownHostException e) {
-//			log.error(e.getMessage());
-//		} catch (IOException e) {
-//			log.error(e.getMessage());
-//		}
-    	
-    	
-    	
-//		try (Socket socket = new Socket()) {
-//	        socket.connect(site, 20);
-//	    
-//	    } catch (IOException e) {
-//	    	log.error("Unable to ping");
-//	    }
     }
     
     
